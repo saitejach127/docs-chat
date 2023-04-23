8b9b04a5df77398a6c22f2dca8cd8fc5b830e8b2
saitejach127 <saitejach127@gmail.com>
2023-04-23 17:09:34 +0530

    Converted DocChat to class based rendering

--- DocChat.py
+++ DocChat.py
@@ -5,96 +5,115 @@ import tiktoken  # for counting tokens
 from scipy import spatial  # for calculating vector similarities for search
 import os
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+class DocChat:
 
-# models
-EMBEDDING_MODEL = "text-embedding-ada-002"
-GPT_MODEL = "gpt-3.5-turbo"
+    EMBEDDING_MODEL = ""
+    GPT_MODEL = ""
+    EMBEDDING_PATH = ""
+    INTRODUCTION_QUESTION = ''
+    SYSTEM_CONTEXT_MESSAGE = ""
+    DOCUMENT_NAME = ""
+    df = pd.DataFrame()
+    openaikey = ""
 
-# download pre-chunked text and pre-computed embeddings
-# this file is ~200 MB, so may take a minute depending on your connection speed
-EMBEDDING_PATH = "embeddings/budget_embeddings.csv"
-INTRODUCTION_QUESTION = 'Use the below Budget 2023-24 speech document to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."'
-SYSTEM_CONTEXT_MESSAGE = "You answer questions about Budget Speech 2023-24."
-DOCUMENT_NAME = "Budget Doc :"
+    def __init__(
+        self,
+        EMBEDDING_PATH,
+        INTRODUCTION_QUESTION,
+        SYSTEM_CONTEXT_MESSAGE,
+        DOCUMENT_NAME,
+        APIKEY,
+        EMBEDDING_MODEL = "text-embedding-ada-002",
+        GPT_MODEL = "gpt-3.5-turbo",
+    ):
+        self.EMBEDDING_PATH = EMBEDDING_PATH
+        self.INTRODUCTION_QUESTION = INTRODUCTION_QUESTION
+        self.SYSTEM_CONTEXT_MESSAGE = SYSTEM_CONTEXT_MESSAGE
+        self.DOCUMENT_NAME = DOCUMENT_NAME
+        self.EMBEDDING_MODEL = EMBEDDING_MODEL
+        self.GPT_MODEL = GPT_MODEL
+        df = pd.read_csv(EMBEDDING_PATH)
+        df['embedding'] = df['embedding'].apply(ast.literal_eval)
+        self.df = df
+        openai.api_key = APIKEY
 
-df = pd.read_csv(EMBEDDING_PATH)
-
-# convert embeddings from CSV str type back to list type
-df['embedding'] = df['embedding'].apply(ast.literal_eval)
-
-# search function
-def strings_ranked_by_relatedness(
-    query: str,
-    df: pd.DataFrame,
-    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
-    top_n: int = 100
-) :
-    """Returns a list of strings and relatednesses, sorted from most related to least."""
-    query_embedding_response = openai.Embedding.create(
-        model=EMBEDDING_MODEL,
-        input=query,
-    )
-    query_embedding = query_embedding_response["data"][0]["embedding"]
-    strings_and_relatednesses = [
-        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
-        for i, row in df.iterrows()
-    ]
-    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
-    strings, relatednesses = zip(*strings_and_relatednesses)
-    return strings[:top_n], relatednesses[:top_n]
-
-
-def num_tokens(text: str, model: str = GPT_MODEL) -> int:
-    """Return the number of tokens in a string."""
-    encoding = tiktoken.encoding_for_model(model)
-    return len(encoding.encode(text))
-
-
-def query_message(
-    query: str,
-    df: pd.DataFrame,
-    model: str,
-    token_budget: int
-) -> str:
-    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
-    strings, relatednesses = strings_ranked_by_relatedness(query, df)
-    introduction = f'{INTRODUCTION_QUESTION}'
-    question = f"\n\nQuestion: {query}"
-    message = introduction
-    for string in strings:
-        next_article = f'\n\n {DOCUMENT_NAME}\n"""\n{string}\n"""'
-        if (
-            num_tokens(message + next_article + question, model=model)
-            > token_budget
-        ):
-            break
-        else:
-            message += next_article
-    return message + question
+    # search function
+    def strings_ranked_by_relatedness(
+            self,
+        query: str,
+        df: pd.DataFrame,
+        relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+        top_n: int = 100
+    ) :
+        """Returns a list of strings and relatednesses, sorted from most related to least."""
+        query_embedding_response = openai.Embedding.create(
+            model=self.EMBEDDING_MODEL,
+            input=query,
+        )
+        query_embedding = query_embedding_response["data"][0]["embedding"]
+        strings_and_relatednesses = [
+            (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+            for i, row in df.iterrows()
+        ]
+        strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+        strings, relatednesses = zip(*strings_and_relatednesses)
+        return strings[:top_n], relatednesses[:top_n]
 
 
-def ask(
-    query: str,
-    df: pd.DataFrame = df,
-    model: str = GPT_MODEL,
-    token_budget: int = 4096 - 500,
-    print_message: bool = False,
-) -> str:
-    """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
-    message = query_message(query, df, model=model, token_budget=token_budget)
-    if print_message:
-        print(message)
-    messages = [
-        {"role": "system", "content": f"{SYSTEM_CONTEXT_MESSAGE}"},
-        {"role": "user", "content": message},
-    ]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0
-    )
-    response_message = response["choices"][0]["message"]["content"]
-    return response_message
+    def num_tokens(self, text: str, model: str) -> int:
+        """Return the number of tokens in a string."""
+        model = self.GPT_MODEL
+        encoding = tiktoken.encoding_for_model(model)
+        return len(encoding.encode(text))
 
-print(ask("Who is the current Finance minister?"))
+
+    def query_message(
+        self,
+        query: str,
+        df: pd.DataFrame,
+        model: str,
+        token_budget: int
+    ) -> str:
+        """Return a message for GPT, with relevant source texts pulled from a dataframe."""
+        strings, relatednesses = self.strings_ranked_by_relatedness(query, df)
+        introduction = f'{self.INTRODUCTION_QUESTION}'
+        question = f"\n\nQuestion: {query}"
+        message = introduction
+        for string in strings:
+            next_article = f'\n\n {self.DOCUMENT_NAME}\n"""\n{string}\n"""'
+            if (
+                self.num_tokens(message + next_article + question, model=model)
+                > token_budget
+            ):
+                break
+            else:
+                message += next_article
+        return message + question
+
+
+    def ask(
+        self,
+        query: str,
+        token_budget: int = 4096 - 500,
+        print_message: bool = False,
+    ) -> str:
+        """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
+        model = self.GPT_MODEL
+        df = self.df
+        message = self.query_message(query, df, model=model, token_budget=token_budget)
+        if print_message:
+            print(message)
+        messages = [
+            {"role": "system", "content": f"{self.SYSTEM_CONTEXT_MESSAGE}"},
+            {"role": "user", "content": message},
+        ]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=0
+        )
+        response_message = response["choices"][0]["message"]["content"]
+        return response_message
+
+docChat = DocChat("embeddings/budget_embeddings.csv", 'Use the below Budget 2023-24 speech document to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."', "You answer questions about Budget Speech 2023-24., ",'Budget Doc :', APIKEY=os.environ["OPENAI_API_KEY"])
+print(docChat.ask("Who is the finance minister of india?"))
