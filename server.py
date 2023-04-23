68e8165ef16113c02cea5e265d265a41a3b07556
nikhilreddy.pottanig <nikhilreddy.pottanig@servicenow.com>
2023-04-23 18:40:48 +0530

    Created working proper UI

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ app = Flask(__name__, template_folder="templates")
 from DocChat import DocChat
 import os
 
-docChat = DocChat("embeddings/budget_embeddings.csv", 'Use the below Budget 2023-24 speech document to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."', "You answer questions about Budget Speech 2023-24., ",'Budget Doc :', APIKEY=os.environ["OPENAI_API_KEY"])
+EMBEDDING_PATH = "embeddings/csm_nltk.csv"
+INTRODUCTION_QUESTION = 'Use the below ServiceNow Customer Service Management Documentation to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."'
+SYSTEM_CONTEXT_MESSAGE = 'You answer questions about ServiceNow Customer Service Management Documentation.'
+DOCUMENT_NAME = 'ServiceNow Documentation :'
+docChat = DocChat(EMBEDDING_PATH, INTRODUCTION_QUESTION, SYSTEM_CONTEXT_MESSAGE,DOCUMENT_NAME, APIKEY=os.environ["OPENAI_API_KEY"])
+# docChat = DocChat("embeddings/budget_embeddings.csv", 'Use the below Budget 2023-24 speech document to answer the subsequent question. If the answer cannot be found in the article, write "I could not find an answer."', "You answer questions about Budget Speech 2023-24., ",'Budget Doc :', APIKEY=os.environ["OPENAI_API_KEY"])
  
 
 @app.route("/", methods=["GET"])
